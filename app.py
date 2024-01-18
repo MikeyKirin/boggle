f52617cb5e17f737d8403edcd1f9e870706cd13c
MikeyKirin <101678214+MikeyKirin@users.noreply.github.com>
2024-01-17 23:37:56 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+from boggle import Boggle
+boggle_game = Boggle()
+
+from flask import Flask, request, render_template, redirect, flash, jsonify, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+# Need to manually create HTML templates folder and tell Flask to render from it
+app = Flask(__name__,template_folder='templates')
+
+app.debug = True
+# Debug Toolbar Requirement
+app.config['SECRET_KEY'] = 'key'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+# Start our routes
+@app.route('/')
+def show_board():
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    return render_template('boggle.html', board=board, 
+                        highscore=highscore,
+                        nplays=nplays)
+
+@app.route('/check-word')
+def check_word():
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result': response})
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    """Receive score, update nplays, update high score if appropriate."""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
+
+@app.route('/restart')
+def restart_game():
+    return redirect('/')
